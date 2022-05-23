7cc9b72eb8067ee8a9d3278dc7fabf78af417581
hugo.rodrigues <hugogabriel.es@gmail.com>
2022-05-23 10:47:32 -0300

    first commit

---
+++ First-project/src/br/com/dio/Firstprogram.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import model.gato;
+
+public class Firstprogram {
+    public static void main(String[] args){
+
+        gato gato = new gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b));*/
+    }
+
+
+
+}
